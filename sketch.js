7e59d99119bc3533408b22b6dca8a9c80ae45bea
HendrikScheeres <hscheeresyt@gmail.com>
2024-09-25 21:08:53 +0200

    Mr. Mole was added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+
+  // the first mole
+  fill(0, 0, 0);
+  ellipse(80, 80, 40);
+  fill(50,50,50);
+  ellipse(98, 85, 30);
+  fill("pink");
+  ellipse(113, 91, 10);
+
+  fill("white")
+  ellipse(97, 83, 6);
+  ellipse(110, 82, 6);
+  
 }
 
 function draw() {
-  background(220);
 }
