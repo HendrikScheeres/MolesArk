f4f4c52ca6ddb3aa91b1231adc2b3607d9423225
HendrikScheeres <hscheeresyt@gmail.com>
2024-09-29 19:53:23 +0200

    added digging behaviour and first attempt to nest building

--- sketch.js
+++ sketch.js
@@ -1,25 +1,44 @@
 class Mole {
   constructor() {
-    this.x = random(690);
-    this.y = 50;
+    this.x = random(windowWidth);
+    this.y = 48;
   }
 
   show() {
-
-    noStroke();
     ellipse(this.x, this.y, 6);
+    noStroke();
     fill(255);
-    
   }
   
   step() {
-    
+
+    // if it reaches the bottom stop
+    if (this.y > (windowHeight - 100)) {
+      
+      return;
+
+    };  
     // Third step function
     let xstep = random(-1, 1);
     let ystep = random(0, 0.5);
 
     this.x += xstep;
     this.y += ystep;
+  }
+
+  nest() {
+    let xstep = 0;
+    // 50% to go left or right
+    if (random() < 0.5) {
+      xstep = -0.1;
+    } else {
+      xstep = 0.1;
+    };
+
+    for (let i = 0; i < 50; i++){
+      this.x += xstep;
+    }
+
   }
 }
 
@@ -28,8 +47,8 @@ let mole;
 
 
 function setup() {
-  createCanvas(690, 500);
-
+ 
+  createCanvas(windowWidth, windowHeight);
   // the first mole 
   // fill(0, 0, 0);
   // ellipse(80, 80, 40);
@@ -44,7 +63,7 @@ function setup() {
 
   // Ground
   fill("#1B0000");
-  rect(0, 50, 1000,500);
+  rect(0, 50, windowWidth, windowHeight);
   noStroke();
 
   // create a walker
@@ -56,4 +75,9 @@ function draw() {
   // Mouse Mole
   mole.show();
   mole.step();
+
+  if (mole.y > windowHeight/2){
+    mole.nest();
+    
+  }
 }
